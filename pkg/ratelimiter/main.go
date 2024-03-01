fb33115d3159e186db56fc92c71e55634ed74a7a
João Guilherme <joao.sczip@hotmail.com>
2024-03-01 08:43:43 -0300

    set client rate limiter as private

--- pkg/ratelimiter/main.go
+++ pkg/ratelimiter/main.go
@@ -11,7 +11,7 @@ var (
 )
 
 type RateLimiter struct {
-	Clients map[string]*ClientRateLimiter
+	clients map[string]*clientRateLimiter
 }
 
 type RateLimiterConfig struct {
@@ -20,7 +20,7 @@ type RateLimiterConfig struct {
 }
 
 func NewRateLimiter() *RateLimiter {
-	manager := &RateLimiter{Clients: make(map[string]*ClientRateLimiter)}
+	manager := &RateLimiter{clients: make(map[string]*clientRateLimiter)}
 
 	go manager.clearRequests()
 
@@ -30,82 +30,80 @@ func NewRateLimiter() *RateLimiter {
 func (r *RateLimiter) clearRequests() {
 	for {
 		time.Sleep(1 * time.Second)
-		for _, client := range r.Clients {
+		for _, client := range r.clients {
 			client.clearRequests()
 		}
 	}
 }
 
 func (r *RateLimiter) HandleRequest(ip string, config RateLimiterConfig) error {
-	clients := r.Clients
+	clients := r.clients
 	mux := sync.Mutex{}
 
 	if _, found := clients[ip]; !found {
-		clients[ip] = NewClientLimiter(config.RequestesPerSecond, config.BlockUserFor)
+		clients[ip] = newClientLimiter(config.RequestesPerSecond, config.BlockUserFor)
 	}
 
 	mux.Lock()
 	defer mux.Unlock()
-	clients[ip].LastSeen = time.Now()
 
-	if clients[ip].IsBlocked() {
-		if clients[ip].HasBlockingExpired() {
-			clients[ip].ResetBlock()
+	if clients[ip].isBlocked() {
+		if clients[ip].hasBlockingExpired() {
+			clients[ip].resetBlock()
 		} else {
 			return errMaxRequests
 		}
 	}
 
-	clients[ip].TotalRequests += 1
+	clients[ip].totalRequests += 1
 
-	if clients[ip].ShouldBlock() {
-		clients[ip].Block()
+	if clients[ip].shouldBlock() {
+		clients[ip].block()
 		return errMaxRequests
 	}
 
 	return nil
 }
 
-type ClientRateLimiter struct {
-	RequestsPerSecond int
-	BlockUserFor      time.Duration
-	LastSeen          time.Time
-	Blocked           bool
-	BlockedAt         time.Time
-	TotalRequests     int
+type clientRateLimiter struct {
+	requestsPerSecond int
+	blockUserFor      time.Duration
+	blocked           bool
+	blockedAt         time.Time
+	totalRequests     int
 }
 
-func NewClientLimiter(rps int, blockDuration time.Duration) *ClientRateLimiter {
-	return &ClientRateLimiter{
-		RequestsPerSecond: rps,
-		BlockUserFor:      blockDuration,
+func newClientLimiter(rps int, blockDuration time.Duration) *clientRateLimiter {
+	return &clientRateLimiter{
+		requestsPerSecond: rps,
+		blockUserFor:      blockDuration,
 	}
 }
 
-func (c *ClientRateLimiter) clearRequests() {
-	if c.TotalRequests <= c.RequestsPerSecond {
-		c.TotalRequests = 0
+func (c *clientRateLimiter) clearRequests() {
+	if c.totalRequests <= c.requestsPerSecond {
+		c.totalRequests = 0
 	}
 }
 
-func (c *ClientRateLimiter) IsBlocked() bool {
-	return c.Blocked
+func (c *clientRateLimiter) isBlocked() bool {
+	return c.blocked
 }
 
-func (c *ClientRateLimiter) HasBlockingExpired() bool {
-	return time.Since(c.BlockedAt) > c.BlockUserFor
+func (c *clientRateLimiter) hasBlockingExpired() bool {
+	return time.Since(c.blockedAt) > c.blockUserFor
 }
 
-func (c *ClientRateLimiter) ResetBlock() {
-	c.Blocked = false
-	c.TotalRequests = 0
+func (c *clientRateLimiter) resetBlock() {
+	c.blocked = false
+	c.totalRequests = 0
 }
 
-func (c *ClientRateLimiter) ShouldBlock() bool {
-	return c.TotalRequests > c.RequestsPerSecond
+func (c *clientRateLimiter) shouldBlock() bool {
+	return c.totalRequests > c.requestsPerSecond
 }
 
-func (c *ClientRateLimiter) Block() {
-	c.Blocked = true
-	c.BlockedAt = time.Now()
+func (c *clientRateLimiter) block() {
+	c.blocked = true
+	c.blockedAt = time.Now()
 }
